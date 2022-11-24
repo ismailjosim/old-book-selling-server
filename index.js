a326b35f7cedadda790a96b222efbd2c56185a1a
JC DESIGN <ismailjosim99@gmail.com>
2022-11-24 23:12:15 +0600

    received single category id and send relevent data from database

---
+++ index.js
@@ -0,0 +1,109 @@
+require('dotenv').config();
+require('colors');
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+// TODO: Middleware
+app.use(cors());
+app.use(express.json())
+
+
+// section: URI & Create Client
+const uri = `mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASSWORD }@cluster0.s9x13go.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// section: Database connect Function
+const dbConnect = async () => {
+    try {
+        await client.connect();
+        console.log("Database Connected!");
+    } catch (error) {
+        console.log(error.name, error.message);
+    }
+}
+dbConnect()
+
+// TODO: Database Collection
+const CategoriesCollection = client.db('oldBookCenter').collection('categories');
+const CategoryCollection = client.db('oldBookCenter').collection('category');
+
+
+
+
+// TODO - 00 : General server Checking
+app.get('/', (req, res) => {
+    res.send(`<div>old Book Server Connected 🎉</div>`)
+})
+
+
+// TODO: 01: Get all categories
+
+app.get('/categories', async (req, res) => {
+    try {
+        const query = {}
+        const categories = await CategoriesCollection.find(query).toArray();
+
+        res.send({
+            success: true,
+            categories: categories
+
+        })
+
+    } catch (error) {
+        res.send({
+            success: false,
+            error: error.message
+        })
+    }
+})
+
+
+// TODO: send single data according to categories data.
+app.get('/category/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const query = { categories_id: parseInt(id) }
+        const books = await CategoryCollection.find(query).toArray()
+
+        res.send({
+            success: true,
+            books: books
+        })
+
+
+    } catch (error) {
+        res.send({
+            success: false,
+            error: error.message
+        })
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// listen app
+app.listen(port, () => console.log(`Server Running on Port ${ port }`.random.bold))
